c955116118e4542153fae276f489c95e10398328
Pavel Hodek <pavel.hodek@gmail.com>
2014-06-22 10:11:26 +0200

    initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by HOD on 19. 6. 2014.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
