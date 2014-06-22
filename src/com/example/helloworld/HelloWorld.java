9eeac0c697e028e15e47a45b5675f20d79d270f8
Pavel Hodek <pavel.hodek@gmail.com>
2014-06-22 10:25:20 +0200

    Revision 2 – the Personal Greeting functionality implemented

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,10 +1,21 @@
 package com.example.helloworld;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by HOD on 19. 6. 2014.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        //System.out.println("Hello world!");
+
+        BufferedReader buff = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.print("What's your name? ");
+        System.out.flush();
+        String s = buff.readLine();
+        System.out.printf("Hello, %s!", s);
     }
 }
